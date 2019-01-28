d02368ac167e92b6136012dfd78b5ac262c73ba8
Campbell Bartlett <campbellb@yambay.com>
2019-01-28 21:24:08 +1100

    Changes to run on Heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
