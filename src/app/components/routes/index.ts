6b4ee5c175b5a98880fd3a3c3a6a5cb2229e6ae8
Campbell Bartlett <38512252+campbellbartlett@users.noreply.github.com>
2019-06-16 14:27:16 +1000

    Clean up modules -  (#47)

--- src/app/components/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { NewsPageComponent } from './news-page/news-page.component';
-import { AboutPageComponent } from './about-page/about-page.component'
-import { HomePageComponent } from './home-page/home-page.component'
-
-export {
-    AboutPageComponent,
-    HomePageComponent,
-    NewsPageComponent
-}
