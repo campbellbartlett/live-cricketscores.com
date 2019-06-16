39f5783b740256f3609e1f9f940ca6974c255664
campbellbartlett <campbellb@yambay.com>
2019-06-16 15:24:14 +1000

    #28 Added match date and time to score card header

--- src/app/components/pipe/runs-wickets-declared/runs-wickets-declared.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RunsWicketsDeclaredPipe } from './runs-wickets-declared.pipe';
-
-describe('RunsWicketsDeclaredPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RunsWicketsDeclaredPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
